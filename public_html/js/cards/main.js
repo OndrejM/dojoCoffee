378c62a2d2e4817363a2333bb9529b1d273e33e0
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2013-11-20 12:24:23 +0100

    Improved data binding and reduced coupling in cards.html

--- public_html/js/cards/main.js
+++ public_html/js/cards/main.js
@@ -2,30 +2,42 @@ define(["dojo/when", "dojo/parser", "dojox/mvc/getStateful", "dojo/Stateful", "d
     , "dojox/fx/flip", "dojo/_base/config"
     , "dojo/domReady!", "dojo/ready", "widgets/Card"], 
 function(when, parser, getStateful, Stateful, $, $cls, fx, config) {
-    var global = this;
-    console.log('INIT');
-    if (config.isDev()) {
-        console.log('Main started...');
-    }
-    initModel();
-    when(parser.parse(), function() {
-        initCard();
-    }, function(err) {
-        console.log(err);
-    });
     
-    function initModel() {
-        var model = getStateful({value: 'value1', displayedCard : {text : "Card text"}});
-        global.model = model;
-    }
+    return function () {
+        // public
+        self = this;
+        self.model = {};
+        self.initCard = initCard;
+        self.start = start;
+        
+        // construct
+        console.debug('CardsMain started...');
+        initModel();
+        
+        function initModel() {
+            self.model = getStateful({value: 'value1', displayedCard : {text : "Card text"}});
+        }
     
-    function initCard() {
-        $('#card').on("click", function(e) {
-            var elem = e.target;
-            $cls.toggle(elem, "red");
-            $cls.toggle(elem, "blue");
-            fx.flip({node: elem}).play();
-            global.model.get('displayedCard').set({text: "Changed card text"});
-        });
+        function initCard(query) {
+            $(query).on("click", function(e) {
+                var elem = e.target;
+                $cls.toggle(elem, "red");
+                $cls.toggle(elem, "blue");
+                self.model.get('displayedCard').set({text: "Changed card text"});
+                fx.flip({node: elem}).play();
+            });
+        }
+
+        function start(callback) {
+            when(parser.parse(), function() {
+                console.debug('Page parsed');
+                callback();
+            }, function(err) {
+                console.error(err);
+            });
+
+        }
+    
     }
+
 });
