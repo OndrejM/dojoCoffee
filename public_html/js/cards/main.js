94ed4c4a0605be16f8098446be49b7fcb5ec1e85
ondro <ondro@Ondro-PC.mshome.net>
2013-11-22 00:35:53 +0100

    Changed adding onclick listener to declarative way

--- public_html/js/cards/main.js
+++ public_html/js/cards/main.js
@@ -7,30 +7,33 @@ function(when, parser, getStateful, Stateful, $, $cls, fx, config) {
         // public
         self = this;
         self.model = {};
-        self.initCard = initCard;
+        self.control = {};
         self.start = start;
         
         // construct
-        console.debug('CardsMain started...');
+        console.debug('CardsMain initializing...');
         initModel();
+        initControllers();
         
         function initModel() {
             self.model = getStateful({value: 'value1', displayedCard : {text : "Card text"}});
         }
-    
-        function initCard(query) {
-            $(query).on("click", function(e) {
-                var elem = e.target;
-                $cls.toggle(elem, "red");
-                $cls.toggle(elem, "blue");
-                self.model.get('displayedCard').set({text: "Changed card text"});
-                fx.flip({node: elem}).play();
-            });
-        }
 
+        function initControllers() {
+            self.control.card = {
+                onclick: function(elem) {
+                    console.debug('Card click');
+                    $cls.toggle(elem, "red");
+                    $cls.toggle(elem, "blue");
+                    self.model.get('displayedCard').set({text: "Changed card text"});
+                    fx.flip({node: elem}).play();
+                }
+            };
+        }
+        
         function start(callback) {
             when(parser.parse(), function() {
-                console.debug('Page parsed');
+                console.debug('CardsMain starting');
                 callback();
             }, function(err) {
                 console.error(err);
--- public_html/js/widgets/common/_ContentPropertyMixin.js
+++ public_html/js/widgets/common/_ContentPropertyMixin.js
@@ -5,13 +5,13 @@
 
 define(["dojo/_base/declare"], function(declare) {
     var _mixin = declare("widgets.common._ContentPropertyMixin", null, {
-        content: "Card content",
+        content: "",
         _getContentAttr: function() {
             return this.containerNode.innerHTML;
         },
         _setContentAttr: function(value) {
             this.containerNode.innerHTML = value;
-            console.log('set content')
+            console.debug('set content')
         }
     });
     return _mixin;
