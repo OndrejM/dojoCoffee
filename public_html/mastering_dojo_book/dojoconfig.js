b14ea9794c1580da91880592660beaa80ab1a17d
OpenShift guest <ondrej.mihalyi@gmail.com>
2014-02-10 11:40:59 -0500

    According to Mastering Dojo book - added demo of improved form (chapter 2.4)

---
+++ public_html/mastering_dojo_book/dojoconfig.js
@@ -0,0 +1,26 @@
+(function(global) {
+    global.dojoConfig = {
+        parseOnLoad: true,
+        async: 1,
+        packages: [
+            {
+                name: 'app',
+                location: localBase() + 'js/app'
+            },
+        ]
+    };
+
+    /* To build path to local package when not relative to dojo.js file.
+     * Returns base path computed from this script path (alternatively browser location path)
+     @return Base path to local packages, to be prefixed before location in dojo package config
+     */
+    function localBase() {
+        if (window.dojoLocalBaseFromJSLocation) {
+            var scripts = document.getElementsByTagName("script"),
+                src = scripts[scripts.length-1].src;
+            return src.replace(/\/[^/]*$/, '') + '/';
+        } else {
+            return window.location.pathname.replace(/\/[^/]*$/, '') + '/';
+        }
+    }
+})(window);
---
+++ public_html/mastering_dojo_book/js/app/index.js
@@ -0,0 +1,4 @@
+define(["dojo/dom", "dojo/html"], function(dom, html) {
+    var feedbackTag = dom.byId("feedback");
+    html.set(feedbackTag, "DOJO Loaded!");
+    });
